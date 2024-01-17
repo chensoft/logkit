b8460df6552a50039a9f1613cb19a98f63da1003
Jian Chen <admin@chensoft.com>
2024-01-17 20:42:54 +0800

    encode: format float

--- src/encode.rs
+++ src/encode.rs
@@ -7,7 +7,28 @@ pub trait Encode: Sync + Send {
 
 impl Encode for i32 {
     fn encode(&self, buf: &mut Vec<u8>) {
+        // todo
         let _ = itoa::write(buf, *self);
+    }
+}
+
+impl Encode for f32 {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        buf.reserve(24);
+        unsafe {
+            let len = ryu::raw::f2s_buffered_n(*self, buf.as_mut_ptr().add(buf.len()));
+            buf.set_len(buf.len() + len);
+        }
+    }
+}
+
+impl Encode for f64 {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        buf.reserve(24);
+        unsafe {
+            let len = ryu::raw::d2s_buffered_n(*self, buf.as_mut_ptr().add(buf.len()));
+            buf.set_len(buf.len() + len);
+        }
     }
 }
 
@@ -26,4 +47,9 @@ impl Encode for String {
     fn encode(&self, buf: &mut Vec<u8>) {
         self.as_str().encode(buf)
     }
+}
+
+#[test]
+fn test() {
+
 }
